0f927829057a09c1bff647193a4be151d5065fa6
zechiel666 <zahradehghanizade@gmail.com>
2025-02-02 01:59:38 +0330

    add login required functionality
    after login redirect to course page.

--- course_selection/courses/views.py
+++ course_selection/courses/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class CourseListView(ListView): #loggin requierd add later
+class CourseListView(LoginRequiredMixin, ListView):
     model = Course
     template_name = 'course_list.html'
     context_object_name = 'courses'
+    login_url = '/login/'  # Redirect to login page if not authenticated
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['departments'] = Department.objects.all()  # Send department list
+        context['user'] = self.request.user  # Pass logged-in user's name
         return context
--- course_selection/user/views.py
+++ course_selection/user/views.py
@@ -28,7 +28,7 @@ class CustomLoginView(LoginView):
     template_name = 'user/registration/login.html'  # Set full path to template
 
     def get_success_url(self):
-        return reverse_lazy('#')  # Redirect after login *add later
+        return reverse_lazy('course_list')  # Redirect after login 
     
     
 def password_reset(request):
