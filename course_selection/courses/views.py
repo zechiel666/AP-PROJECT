566488910da43e15ea4cfaa1e6334f706b52f7a3
radinnaimi <ra2005.83b@gmail.com>
2025-02-04 22:34:33 +0330

    check to select course is unique

--- course_selection/courses/views.py
+++ course_selection/courses/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views.generic import ListView
 from .models import *
 from django.contrib.auth.mixins import LoginRequiredMixin
+from user.models import selectedcourse
+from django.contrib import messages
 
 class CourseListView(LoginRequiredMixin, ListView):
     model = Course
@@ -13,4 +15,14 @@ class CourseListView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['departments'] = Department.objects.all()  # Send department list
         context['user'] = self.request.user  # Pass logged-in user's name
+        context['selectedcourse'] = selectedcourse.objects.filter(user = self.request.user)
         return context
+    
+    def post(self, request,*args, **kwargs):
+        course_id = request.post.get('code')
+        course = Course.objects.get(code=course_id)
+        user = request.user
+
+        if selectedcourse.objects.filter(use=user, course=course).exists():
+            messages.erorr(request, 'این درس انتخاب شده است')
+            return redirect('course_list')
