480b7c27c910cfa733f571dc37a3ed2d21d73661
radinnaimi <ra2005.83b@gmail.com>
2025-02-05 18:40:11 +0330

    adding remove url

--- course_selection/courses/urls.py
+++ course_selection/courses/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns=[
     path('courses/', CourseListView.as_view(), name='course_list')
+    path('remove-course/', Removeselectedcourse.as_view(), name='remove_selected_course')
 ]
