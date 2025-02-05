8ada90122661c97e14ba80925d1a864b63e73483
radinnaimi <ra2005.83b@gmail.com>
2025-02-05 18:54:40 +0330

    debugging remove button html

--- course_selection/courses/urls.py
+++ course_selection/courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('courses/', CourseListView.as_view(), name='course_list')
+    path('courses/', CourseListView.as_view(), name='course_list'),
     path('remove-course/', Removeselectedcourse.as_view(), name='remove_selected_course')
 ]
