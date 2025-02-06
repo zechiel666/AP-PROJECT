f65da2fc2cf9ec8a43260caf052a14d0541dbdac
radinnaimi <ra2005.83b@gmail.com>
2025-02-06 19:44:12 +0330

    again checking that user is not student

--- course_selection/teacher/views.py
+++ course_selection/teacher/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
+from django.shortcuts import render , redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
+class TeacherDashboardView(LoginRequiredMixin, View):
+    def get(self, request):
+        if not request.user.is_teacher:  
+            return redirect('/select-courses/')  
+
+        return render(request, 'teacher_dashboard.html')
+    
