23d8f00999fd8aa94ba669d981245fc5bbe69dc4
radinnaimi <ra2005.83b@gmail.com>
2025-02-06 19:32:20 +0330

    checking that students cant login to admin

--- course_selection/teacher/admin.py
+++ course_selection/teacher/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import path
 
-# Register your models here.
+class Adminsite(admin.AdminSite):
+    def has_permission(self, request):
+        return request.user.is_authenticated and request.user.is_teacher
+    
+
+admin_site = Adminsite (name='admin_site')
+
+urlpatterns = [
+    path('admin/', admin_site.urls),
+]
